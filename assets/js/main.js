7e67959db70f0d267d95d08e56f015fc2ea81b88
austinbhale <haleau@live.unc.edu>
2018-12-27 15:47:37 -0500

    More website pages and additions.

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,27 +8,35 @@ $(function() {
   ) {
     isMobile = true;
   }
-
-    (isMobile) ? $('#parallax-mobile').css("display", "block") : $('#parallax').css("display", "block");
-    if (isMobile) {$('#parallax-ctr').removeClass("parallax-container").addClass("noparallax-container");}
+  for (var i = 0; i <= 2; i++) {
+    if (isMobile) {
+      $("#parallax-mobile" + i).css("display", "block");
+      $("#parallax-ctr" + i)
+        .removeClass("parallax-container")
+        .addClass("noparallax-container");
+    } else {
+      $("#parallax" + i).css("display", "block");
+    }
+  }
 });
 
-var elementsValid = document.getElementsByClassName('valid');
-var elementsInvalid = document.getElementsByClassName('invalid');
-var valids = false; var invalids = false; var received = false;
-$('#gform').on('submit', function(e) {
-     
-    (elementsValid.length > 3) ? valids = true : valids = false;
-    (elementsInvalid.length == 0) ? invalids = true : invalids = false;
-    
-    if (valids && invalids) {
-        $('#gform *').fadeOut(2000, function() {
-            if (!received) {
-                $('#gform').prepend('Thanks! I\'ll be in touch with you soon.');
-                received = true;
-            }
-        });
-    } else {
-        alert("Please fill in all fields.");
-    }
+var elementsValid = document.getElementsByClassName("valid");
+var elementsInvalid = document.getElementsByClassName("invalid");
+var valids = false;
+var invalids = false;
+var received = false;
+$("#gform").on("submit", function(e) {
+  elementsValid.length > 3 ? (valids = true) : (valids = false);
+  elementsInvalid.length == 0 ? (invalids = true) : (invalids = false);
+
+  if (valids && invalids) {
+    $("#gform *").fadeOut(2000, function() {
+      if (!received) {
+        $("#gform").prepend("Thanks! I'll be in touch with you soon.");
+        received = true;
+      }
+    });
+  } else {
+    alert("Please fill in all fields.");
+  }
 });
